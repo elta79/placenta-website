8e7e425b7245266664d03c426040834e4202c2ae
elta79 <earnold@outlook.com>
2023-05-09 16:36:12 -0400

    Unhide Public Key

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -103,7 +103,7 @@ function Contact() {
         console.log(service)
         console.log(condition)
 
-        emailjs.sendForm('contact_service', 'contact_form', form.current, `${process.env.REACT_APP_EMAIL_KEY}`)
+        emailjs.sendForm('contact_service', 'contact_form', form.current, 'Ksv0B-69iF1uQVgg1')
             .then((result) => {
                 console.log(result.text)
             }, (error) => {
