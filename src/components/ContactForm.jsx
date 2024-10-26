fc597e80c8da669e8ae8ee21013ad48f33b10ea1
elta79 <elta7679@gmail.com>
2024-10-26 18:53:23 -0400

    fix contact form format
    Fix VScode format

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -112,161 +112,161 @@ const ContactForm =()=>{
   }    
   return(
     <>
-                  <form ref={form} onSubmit={handleSubmit}>
-                <h3 className='title--contact'>Contact Form</h3>
-                <div className="grid-container-name">
-                    <span id='firstName'>First Name</span>
-                    <input
-                        type='text'
-                        className='firstName'
-                        name='firstName'
-                        value={formData.firstName}
-                        onChange={handleChange}
-                        required
-                    />
-                    <span id='lastName'>Last Name</span>
-                    <input
-                        type='text'
-                        className='lastName'
-                        name='lastName'
-                        value={formData.lastName}
-                        onChange={handleChange}
-                        required
-                    />                    
-                </div>  
-                <span>Email</span>
-                <input
-                    type='email'
-                    className='email'
-                    name='email'
-                    value={formData.email}
-                    onChange={handleChange}
-                    required
-                />
-                <span>Phone Number</span>
-                <input
-                    placeholder='xxx-xxx-xxxx'
-                    type='text'
-                    className='phone'
-                    name='phone'
-                    value={formData.phone}
-                    onChange={handleChange}
-                    required
-                />
-                <span>Address</span>
-                <input
-                    type='text'
-                    className='address'
-                    name='address'
-                    value={formData.address}
-                    onChange={handleChange}
-                    required
-                />                
-                <div className='grid-container-address'>
-                    <span id='city'>City</span>
-                    <input
-                        type='text'
-                        className='city'
-                        name='city'
-                        value={formData.city}
-                        onChange={handleChange}
-                        required                        
-                    />
-                    <span id='state'>State</span>
-                    <input
-                        type='text'
-                        className='state'
-                        name='state'
-                        value={formData.state}
-                        onChange={handleChange}
-                        required
-                    />
-                    <span id='zip'>Zip Code</span>
-                    <input
-                        type='text'
-                        className='zip'
-                        name='zip'
-                        value={formData.zip}
-                        onChange={handleChange}
-                        required
-                    />                   
-                </div>
-                <span>Estimated Due Date</span>
-                <input
-                    type='date'
-                    className='edd'
-                    name='edd'
-                    value={formData.edd}
-                    onChange={handleChange}
-                    required
-                />
-                <span>Scheduled C-section Date</span>
-                <input
-                    type='date'
-                    className='csec'
-                    name='csec'
-                    value={formData.csec}
-                    onChange={handleChange}                    
-                />               
-                <hr />
-                <br />
-                <span>Please choose at least one service:</span>
-                <Select
-                    options={options}
-                    onChange={handleChangeService}
-                    name='service'
-                    className='service'
-                    value={formData.service}
-                    isMulti
-                    styles={customStyles}
-                />
-                <span>Please select any of the conditions that you have been diagnosed with during this pregnancy:</span>
-                <Select 
-                    options={conditions}
-                    onChange={handleChangeConditions}
-                    name='condition'
-                    className='condition'
-                    value={formData.condition}
-                    isMulti
-                    styles={customStyles}                    
-                />    
-                {/* pass prop setOpenModal as closeModal */}
-                {openModal && <Modal closeModal={setOpenModal}/>}
-                <span>Where do you plan to give birth?:</span>
-                <select 
-                    id='location'
-                    name='location'
-                    value={formData.location}
-                    onChange={handleChange}
-                >
-                    <option value='home'>Home</option>
-                    <option value='nch'>North Collier Hospital</option>
-                    <option value='healthPark'>Health Park</option>
-                    <option value='other'>Other</option>
-                </select>
-                <span>What is your Baby's sex?:</span>
-                <select 
-                    id='sex'
-                    name='sex'
-                    value={formData.sex}
-                    onChange={handleChange}
-                >
-                    <option value='surprise'>Surprise</option>
-                    <option value='girl'>Girl</option>
-                    <option value='boy'>Boy</option>
-                </select>
-                <textarea
-                    value={formData.comments}
-                    onChange={handleChange}
-                    name='comments'
-                    className='comments'
-                    placeholder='Anything else you would like me to know?'
-                />                
-                <div className='button'>
-                    <button id='button--submit'>Submit</button>
-                </div>                
+      <form ref={form} onSubmit={handleSubmit}>
+        <h3 className='title--contact'>Contact Form</h3>
+        <div className="grid-container-name">
+            <span id='firstName'>First Name</span>
+            <input
+                type='text'
+                className='firstName'
+                name='firstName'
+                value={formData.firstName}
+                onChange={handleChange}
+                required
+            />
+            <span id='lastName'>Last Name</span>
+            <input
+                type='text'
+                className='lastName'
+                name='lastName'
+                value={formData.lastName}
+                onChange={handleChange}
+                required
+            />                    
+        </div>  
+        <span>Email</span>
+        <input
+            type='email'
+            className='email'
+            name='email'
+            value={formData.email}
+            onChange={handleChange}
+            required
+        />
+        <span>Phone Number</span>
+        <input
+            placeholder='xxx-xxx-xxxx'
+            type='text'
+            className='phone'
+            name='phone'
+            value={formData.phone}
+            onChange={handleChange}
+            required
+        />
+        <span>Address</span>
+        <input
+            type='text'
+            className='address'
+            name='address'
+            value={formData.address}
+            onChange={handleChange}
+            required
+        />                
+        <div className='grid-container-address'>
+            <span id='city'>City</span>
+            <input
+                type='text'
+                className='city'
+                name='city'
+                value={formData.city}
+                onChange={handleChange}
+                required                        
+            />
+            <span id='state'>State</span>
+            <input
+                type='text'
+                className='state'
+                name='state'
+                value={formData.state}
+                onChange={handleChange}
+                required
+            />
+            <span id='zip'>Zip Code</span>
+            <input
+                type='text'
+                className='zip'
+                name='zip'
+                value={formData.zip}
+                onChange={handleChange}
+                required
+            />                   
+        </div>
+        <span>Estimated Due Date</span>
+        <input
+            type='date'
+            className='edd'
+            name='edd'
+            value={formData.edd}
+            onChange={handleChange}
+            required
+        />
+        <span>Scheduled C-section Date</span>
+        <input
+            type='date'
+            className='csec'
+            name='csec'
+            value={formData.csec}
+            onChange={handleChange}                    
+        />               
+        <hr />
+        <br />
+        <span>Please choose at least one service:</span>
+        <Select
+            options={options}
+            onChange={handleChangeService}
+            name='service'
+            className='service'
+            value={formData.service}
+            isMulti
+            styles={customStyles}
+        />
+        <span>Please select any of the conditions that you have been diagnosed with during this pregnancy:</span>
+        <Select 
+            options={conditions}
+            onChange={handleChangeConditions}
+            name='condition'
+            className='condition'
+            value={formData.condition}
+            isMulti
+            styles={customStyles}                    
+        />    
+        {/* pass prop setOpenModal as closeModal */}
+        {openModal && <Modal closeModal={setOpenModal}/>}
+        <span>Where do you plan to give birth?:</span>
+        <select 
+            id='location'
+            name='location'
+            value={formData.location}
+            onChange={handleChange}
+        >
+            <option value='home'>Home</option>
+            <option value='nch'>North Collier Hospital</option>
+            <option value='healthPark'>Health Park</option>
+            <option value='other'>Other</option>
+        </select>
+        <span>What is your Baby's sex?:</span>
+        <select 
+            id='sex'
+            name='sex'
+            value={formData.sex}
+            onChange={handleChange}
+        >
+            <option value='surprise'>Surprise</option>
+            <option value='girl'>Girl</option>
+            <option value='boy'>Boy</option>
+        </select>
+        <textarea
+            value={formData.comments}
+            onChange={handleChange}
+            name='comments'
+            className='comments'
+            placeholder='Anything else you would like me to know?'
+        />                
+        <div className='button'>
+            <button id='button--submit'>Submit</button>
+        </div>                
                           
-            </form>
+      </form>
     </>
   )
 }
